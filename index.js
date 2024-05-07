6ddc23a43f42a67fc44f3175b64447d789779a70
Gourav Ghuge <you@example.com>
2024-05-07 23:32:19 +0530

    start the project

---
+++ index.js
@@ -0,0 +1,20 @@
+import app from './src/app.js';
+import connectDB from './src/database/database.js';
+
+const PORT = 3000 || process.env.PORT;
+
+connectDB()
+
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+
+.catch((error) => {
+
+    console.log("Error in connecting the database ans connecting to express server", error);
+    process.exit(1);
+});
+
+
---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/', ( req, res ) => {
+   
+    res.send('Hello World!');
+});
+
+
+
+
+
+export default app;
